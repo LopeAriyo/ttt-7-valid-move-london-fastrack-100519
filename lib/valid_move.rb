4334a1c7ffe6289d97879cfea350255835ece75f
Lopè Ariyo <lopeariyo@gmail.com>
2019-10-06 11:50:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
- if !position_taken?(board,index) && index >= 0 && index <= 8 then #if the position is not take aka position_taken? returns false  then the move is valid
+ if !position_taken?(board,index) #&& index >= 0 && index <= 8 then #if the position is not take aka position_taken? returns false  then the move is valid
   return true
 #elsif !(index >= 0 && index <= 8)  #if position is not on the board then the move is invalid - so if index is greater than 8 or less than zero then the move is invalid
  #return false this doesn't work because a true value has already been returned so nothing else will get evaluated
